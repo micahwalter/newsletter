849ee2f7285bf4bdfa81bc131dfc0535c1bfe268
Micah Walter <micah@hey.com>
2021-07-27 17:51:44 +0000

    store emails in dynamodb

--- subscribe/go.mod
+++ subscribe/go.mod
@@ -2,4 +2,7 @@ module github.com/micahwalter/newsletter/subscribe
 
 go 1.16
 
-require github.com/aws/aws-lambda-go v1.25.0
+require (
+	github.com/aws/aws-lambda-go v1.25.0
+	github.com/aws/aws-sdk-go v1.40.8
+)
--- subscribe/main.go
+++ subscribe/main.go
@@ -4,14 +4,23 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/mail"
+	"os"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 type rsp struct {
 	Email string `json:"email"`
 	Stat  string `json:"stat"`
+}
+
+type Subscriber struct {
+	Email string
 }
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
@@ -34,6 +43,31 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	}
 
 	// attempt to insert the email into the database
+	sess := session.Must(session.NewSessionWithOptions(session.Options{
+		SharedConfigState: session.SharedConfigEnable,
+	}))
+
+	svc := dynamodb.New(sess)
+
+	data := Subscriber{
+		Email: email,
+	}
+
+	av, err := dynamodbattribute.MarshalMap(data)
+	if err != nil {
+		exitErrorf("Got error marshalling new item:", av, err)
+	}
+
+	tableName := "Subscribers"
+	input := &dynamodb.PutItemInput{
+		Item:      av,
+		TableName: aws.String(tableName),
+	}
+
+	_, err = svc.PutItem(input)
+	if err != nil {
+		exitErrorf("Got error calling PutItem:", err)
+	}
 
 	// parrot back the email for testing
 	body := rsp{
@@ -49,6 +83,11 @@ func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 func valid(email string) bool {
 	_, err := mail.ParseAddress(email)
 	return err == nil
+}
+
+func exitErrorf(msg string, args ...interface{}) {
+	fmt.Fprintf(os.Stderr, msg+"\n", args...)
+	os.Exit(1)
 }
 
 func main() {
