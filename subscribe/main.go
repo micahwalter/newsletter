b1149783642c2fec41047691a4e04889cab9f0d6
Micah Walter <micah@hey.com>
2021-07-27 14:53:35 +0000

    validate email address

--- subscribe/main.go
+++ subscribe/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/mail"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -15,23 +16,39 @@ type rsp struct {
 
 func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	fmt.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
-	fmt.Printf("Body size = %d.\n", len(request.Body))
-	fmt.Printf("Email: %s.\n", request.QueryStringParameters["email"])
+	// grab the email from the query string params
+	email := request.QueryStringParameters["email"]
 
-	fmt.Println("Headers:")
-	for key, value := range request.Headers {
-		fmt.Printf("    %s: %s\n", key, value)
+	// validate that its a real email address
+	fmt.Printf("%18s valid: %t\n", email, valid(email))
+
+	if !valid(email) {
+		body := rsp{
+			Email: email,
+			Stat:  "error",
+		}
+
+		b, _ := json.Marshal(body)
+
+		return events.APIGatewayProxyResponse{Body: string(b), StatusCode: 200}, nil
 	}
 
+	// attempt to insert the email into the database
+
+	// parrot back the email for testing
 	body := rsp{
-		Email: request.QueryStringParameters["email"],
+		Email: email,
 		Stat:  "ok",
 	}
 
 	b, _ := json.Marshal(body)
 
 	return events.APIGatewayProxyResponse{Body: string(b), StatusCode: 200}, nil
+}
+
+func valid(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
 }
 
 func main() {
